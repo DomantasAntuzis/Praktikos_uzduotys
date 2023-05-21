09dab263365b956af2954728a254b2f46cb944e5
Domantuzis <antuzisdomantas@gmail.com>
2023-05-21 21:28:24 +0300

    route all routes

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,46 @@ app.use(express.static(__dirname + "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandlingMiddleware);
 
-app.use("/api", productsRoutes);
-app.use("/api", operationsRoutes);
+app.use('/api', productsRoutes);
+app.use('/api', operationsRoutes);
+
+// Route interface
+interface Route {
+  method: string;
+  path: string;
+}
+
+// Retrieve routes
+const routes: Route[] = [];
+
+app._router.stack.forEach((middleware: any) => {
+  if (middleware.route) {
+    const { path, stack } = middleware.route;
+    stack.forEach((route: any) => {
+      const { method } = route;
+      routes.push({
+        method: method.toUpperCase(),
+        path: "/api" + path,
+      });
+    });
+  } else if (middleware.name === 'router') {
+    middleware.handle.stack.forEach((layer: any) => {
+      const baseRoute = layer.route.path;
+      layer.route && layer.route.stack.forEach((route: any) => {
+        const { method } = route;
+        routes.push({
+          method: method.toUpperCase(),
+          path: "/api" + baseRoute
+        });
+      });
+    });
+  }
+});
+
+// all routes
+app.get('/api/routes', (_req: Request, res: Response) => {
+  res.json(routes);
+});
 
 // not implemented routes
 app.use((_req: Request, res: Response) => {
@@ -26,6 +64,7 @@ async function startServer() {
     await sequelize.authenticate();
     logger.info('Connection has been established successfully.');
     console.log('Connection has been established successfully.');
+
     app.listen(PORT, () => {
       logger.info(`Server Running here 👉 https://localhost:${PORT}`);
     });
@@ -36,4 +75,3 @@ async function startServer() {
 }
 
 startServer();
-
